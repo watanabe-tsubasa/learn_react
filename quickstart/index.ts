246bc8652ce910920d9bce20fb34b54d2ae7e324
Tsubasa Watanabe <t.watanabe423@gmail.com>
2023-10-17 07:08:12 +0000

    upd

--- quickstart/index.ts
+++
@@ -1,8 +0,0 @@
-import { data } from "./data";
-import json from "./json.json";
-import text from "./text.txt";
-
-console.log(data);
-console.log(text);
-console.log(json);
-console.log(process.env.FOO);
