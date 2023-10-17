c980fdf9c47e712868e309592c83c18c4aadb1b1
Tsubasa Watanabe <t.watanabe423@gmail.com>
2023-10-17 01:40:32 +0000

    first_commit

---
+++ quickstart/index.ts
@@ -0,0 +1,8 @@
+import { data } from "./data";
+import json from "./json.json";
+import text from "./text.txt";
+
+console.log(data);
+console.log(text);
+console.log(json);
+console.log(process.env.FOO);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# learn_react
+
+reactと言いつつbunのtest
